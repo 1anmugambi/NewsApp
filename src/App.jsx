9b3252ba35b4f6bb364162e7a625c47a1460baf2
Ian Mugambi <ian.mugambi1@student.moringaschool.com>
2024-05-07 14:24:54 +0300

    Mock

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Mock from './Components/Mock/Mock'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <p>News App</p>
+    <Mock/>
     </>
   )
 }
---
+++ src/Components/Mock/Mock.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Mock() {
+  return (
+    <div>mock</div>
+  )
+}
+
+export default Mock
