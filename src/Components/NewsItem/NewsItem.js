f2b8845afadc6f2966a40831a23268da31f392e1
humphrey <writernic89@gmail.com>
2024-05-08 08:36:03 +0300

    Updated NewsItem.js and NewsItem.css

--- src/Components/NewsItem/NewsItem.js
+++ src/Components/NewsItem/NewsItem.js
@@ -1,29 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { searchArticle } from '../Redux/Actions/SearchActions';
+import './NewsItem.css'; // Import custom CSS file for component styling
 
 const NewsItem = () => {
-  const [newsData, setNewsData] = useState([]);
+  const dispatch = useDispatch();
+  const searchResults = useSelector((state) => state.search.articles);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://newsapi.org/v2/everything?q=keyword&apiKey=877963a15492486ca2a85b2c521d2894');
-        const data = await response.json();
-        setNewsData(data.articles);
+        const response = await dispatch(searchArticle('news'));
+        console.log(response); // Debugging: Check response
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
     fetchData();
-  }, []);
+
+    // Cleanup function
+    return () => {
+      // Any cleanup code if needed
+    };
+  }, [dispatch]);
 
   return (
-    <div>
-      {newsData.map((newsItem, index) => (
-        <div key={index}>
-          <h2>{newsItem.title}</h2>
-          <p>{newsItem.description}</p>
-          <img src={newsItem.urlToImage} alt={newsItem.title} />
+    <div className="news-container"> {/* Apply custom container class */}
+      {searchResults.map((result, index) => (
+        <div className="news-item" key={index}> {/* Apply custom item class */}
+          <h2>{result.title}</h2>
+          <p>{result.description}</p>
+          <img src={result.urlToImage} alt={result.title} />
+          {/* Add other news item details as needed */}
         </div>
       ))}
     </div>
