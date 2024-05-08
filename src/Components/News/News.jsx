03318e1c05d71720ddeced2f93c523cbc076d1d7
Justin Onyango <ojustin446@gmail.com>
2024-05-08 14:17:58 +0300

    3rd

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,3 +1,9 @@
 import React from "react";
 import axios from "axios"
 
+function News() {
+    const getNews = () => {
+        axios.get("https://newsapi.org/v2/everything?q=keyword&apiKey=877963a15492486ca2a85b2c521d2894")
+        .then(response => response.json())
+    }
+}
