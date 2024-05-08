1ceb0996771dc96708c78b67d04fab58f0a03136
Ian Mugambi <ian.mugambi1@student.moringaschool.com>
2024-05-08 12:10:09 +0300

    SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Mock from './Components/Mock/Mock'
+import React from 'react';
+import './App.css';
+import Mock from './Components/Mock/Mock';
+import SearchBar from './Components/Search/Index';
 
 function App() {
-  
-
   return (
     <>
       <p>News App</p>
-    <Mock/>
+      <Mock/>
+      <SearchBar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
