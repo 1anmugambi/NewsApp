7649871a39d5ad90e72132d5ce55e24f53425d5f
jeangathoni8 <jeangathoni39@gmail.com>
2024-05-08 11:55:46 +0300

    fianl changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store } from "./Components/Store/Store";
 
 const root = document.getElementById("root");
 const rootElement = createRoot(root);
